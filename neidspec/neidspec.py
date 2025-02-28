d15df4a0d4700d61a45a13b1775fd58e87924a53
tehan <tehanhunter@gmail.com>
2025-02-28 13:03:20 -0500

    sn102

--- neidspec/neidspec.py
+++ neidspec/neidspec.py
@@ -24,6 +24,58 @@ PATH_CCF_MASK = crosscorr.mask.ESPRESSO_M3MASK
 PATH_WAVELENGTH = os.path.join(DIRNAME, "data/hpf/wavelength_solution/LFC_wavecal_scifiber_v2.fits")
 PATH_TARGETS = target.PATH_TARGETS
 
+import numpy as np
+
+
+def rot_int_cmj(w, s, vsini, eps=0.6, nr=10, ntheta=100, dif=0.0):
+    '''
+    A routine to quickly rotationally broaden a spectrum in linear time.
+
+    INPUTS:
+    s - input spectrum
+
+    w - wavelength scale of the input spectrum
+
+    vsini (km/s) - projected rotational velocity
+
+    OUTPUT:
+    ns - a rotationally broadened spectrum on the wavelength scale w
+
+    OPTIONAL INPUTS:
+    eps (default = 0.6) - the coefficient of the limb darkening law
+
+    nr (default = 10) - the number of radial bins on the projected disk
+
+    ntheta (default = 100) - the number of azimuthal bins in the largest radial annulus
+                            note: the number of bins at each r is int(r*ntheta) where r < 1
+
+    dif (default = 0) - the differential rotation coefficient, applied according to the law
+    Omeg(th)/Omeg(eq) = (1 - dif/2 - (dif/2) cos(2 th)). Dif = .675 nicely reproduces the law
+    proposed by Smith, 1994, A&A, Vol. 287, p. 523-534, to unify WTTS and CTTS. Dif = .23 is
+    similar to observed solar differential rotation. Note: the th in the above expression is
+    the stellar co-latitude, not the same as the integration variable used below. This is a
+    disk integration routine.
+
+    '''
+
+    ns = np.copy(s) * 0.0
+    tarea = 0.0
+    dr = 1. / nr
+    for j in range(0, nr):
+        r = dr / 2.0 + j * dr
+        area = ((r + dr / 2.0) ** 2 - (r - dr / 2.0) ** 2) / int(ntheta * r) * (1.0 - eps + eps * np.cos(np.arcsin(r)))
+        for k in range(0, int(ntheta * r)):
+            th = np.pi / int(ntheta * r) + k * 2.0 * np.pi / int(ntheta * r)
+            if dif != 0:
+                vl = vsini * r * np.sin(th) * (1.0 - dif / 2.0 - dif / 2.0 * np.cos(2.0 * np.arccos(r * np.cos(th))))
+                ns += area * np.interp(w + w * vl / 2.9979e5, w, s)
+                tarea += area
+            else:
+                vl = r * vsini * np.sin(th)
+                ns += area * np.interp(w + w * vl / 2.9979e5, w, s)
+                tarea += area
+
+    return ns / tarea
 
 class NEIDSpectrum(object):
     """
@@ -42,7 +94,7 @@ class NEIDSpectrum(object):
 
     def __init__(self, filename, targetname='', deblaze=True, tell_err_factor=1., ccf_redshift=True,
                  sky_err_factor=1., sky_scaling_factor=1.0, verbose=False, setup_he10830=False, rv=0.,
-                 degrade_snr=None):
+                 degrade_snr=None, add_vsini=10):
         self.filename = filename
         self.basename = filename.split(os.sep)[-1]
         self.sky_scaling_factor = sky_scaling_factor
@@ -111,6 +163,8 @@ class NEIDSpectrum(object):
 
         self.sn55 = np.nanmedian(self.f[55 - self.start] / self.e[55 - self.start])
         self.sn56 = np.nanmedian(self.f[56 - self.start] / self.e[56 - self.start])
+        self.sn102 = np.nanmedian(self.f[102 - self.start] / self.e[102 - self.start])
+        print(f'sn102 = {self.sn102}')
         self.sn = self.f / self.e
         if targetname == '':
             targetname = self.object
@@ -139,6 +193,29 @@ class NEIDSpectrum(object):
         # self.hdu.close()
         if setup_he10830:
             self._setup_he10830()
+
+        # if filename == '/Users/tehan/Downloads/neidL2_20210223T123115.fits':
+        #
+        #     # Initialize the broadened flux array
+        #     broadened_flux = np.zeros_like(self.f_debl)
+        #
+        #     # Loop through each spectral order
+        #     for i in range(np.shape(self.w)[0]):
+        #         w_order = self.w[i]  # Wavelength array for this order
+        #         f_order = self.f_debl[i]  # Flux array for this order
+        #
+        #         # Apply rotational broadening using the new function
+        #         broadened_flux[i] = rot_int_cmj(w_order, f_order, vsini=add_vsini)
+        #
+        #     # Update the deblended flux with the broadened flux
+        #     self.f_debl = broadened_flux
+
+    # # Sky mask from sky fiber
+        # sky_threshold = np.nanpercentile(self.f_sky, 95,  axis=1, keepdims=True)  # Top 5% intensity in sky spectrum
+        # sky_mask = self.f_sky <= sky_threshold
+        # self.f = np.where(sky_mask, self.f, np.nan)
+        # self.e = np.where(sky_mask, self.e, np.nan)
+        # self.w = np.where(sky_mask, self.w, np.nan)
 
     def _setup_he10830(self, nmedfilt=7, interp='linear'):
         """
